6b7c2d8f4e422d5570ed335412e75647e24069aa
arichorn <78001398+arichorn@users.noreply.github.com>
2023-08-11 16:27:29 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,11 +1,23 @@
+//
+//  apps.js
+//  altsource-viewer (https://github.com/therealFoxster/altsource-viewer)
+//
+//  Copyright (c) 2023 Foxster.
+//  MIT License.
+//
+
+import { insertNavigationBar } from "./utilities.js";
+import { AppHeader } from "./components/AppHeader.js";
+import { main } from "./main.js";
+
 insertNavigationBar("All Apps");
 
-function main(json) {
+main((json) => {
     // Set tab title
     document.title = `Apps - ${json.name}`;
 
     // Sort apps in decending order of version date (newest first)
-    json.apps.sort((a, b) => (new Date(b.versionDate)).valueOf() - (new Date(a.versionDate)).valueOf());
+    // json.apps.sort((a, b) => (new Date(b.versionDate)).valueOf() - (new Date(a.versionDate)).valueOf());
 
     // Create & insert app items
     json.apps.forEach(app => {
@@ -13,7 +25,7 @@ function main(json) {
 
         let html = `
         <div class="app-container">
-            ${appHeaderHTML(app) }
+            ${AppHeader(app)}
             <p style="text-align: center; font-size: 0.9em;">${app.subtitle ?? ""}</p>`;
         if (app.screenshotURLs) {
             html += `
@@ -28,4 +40,4 @@ function main(json) {
 
         document.getElementById("apps").insertAdjacentHTML("beforeend", html);
     });
-}
+});
